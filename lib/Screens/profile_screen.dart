a69868ffc21f91521c2e59bf1aa2291c7e96cc14
Sarthak Agarwal <57105611+Sarthak-ONS@users.noreply.github.com>
2023-07-15 16:14:37 +0530

    Added Profile Screen

--- lib/Screens/profile_screen.dart
+++ lib/Screens/profile_screen.dart
@@ -1,13 +1,15 @@
+import 'package:ecom/app_colors.dart';
 import 'package:flutter/material.dart';
 
-class ProfileScreen extends StatefulWidget {
+const svgIcons = [
+  "https://img.icons8.com/ios/50/wallet--v1.png",
+  "https://img.icons8.com/ios/50/filled-chat.png",
+  "https://img.icons8.com/dotty/80/token-card-code.png"
+];
+
+class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
-  @override
-  State<ProfileScreen> createState() => _ProfileScreenState();
-}
-
-class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,16 +19,132 @@ class _ProfileScreenState extends State<ProfileScreen> {
         title: const Text('Profile'),
       ),
       body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 1),
-        child: const Column(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('My Account'),
-            Text('9457000000'),
+            const Text(
+              'My Account',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              '9457000000',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 186, 224, 255),
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+              margin: const EdgeInsets.symmetric(vertical: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  buildIconWithLabel(assetName: svgIcons[0], title: 'Wallet'),
+                  buildIconWithLabel(assetName: svgIcons[1], title: 'Support'),
+                  buildIconWithLabel(assetName: svgIcons[2], title: 'Payments'),
+                ],
+              ),
+            ),
+
+            // User Information
+            const Text(
+              'YOUR INFORMATION',
+              style: TextStyle(
+                color: Colors.grey,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            customListTile(
+                icon: Icons.inventory_2_outlined, title: 'Your Orders'),
+            customListTile(
+                icon: Icons.inventory_2_outlined, title: 'Address Book'),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              'YOUR INFORMATION',
+              style: TextStyle(
+                color: Colors.grey,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            customListTile(icon: Icons.share, title: 'Share the app'),
+            customListTile(icon: Icons.info_outline, title: 'About Us'),
+            customListTile(icon: Icons.logout, title: 'Log out'),
           ],
         ),
       ),
     );
   }
+
+  Widget buildIconWithLabel({@required assetName, @required String? title}) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 30,
+          child: Image.network(assetName),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Text(
+          title!,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget customListTile({
+    required IconData icon,
+    required String title,
+  }) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+      margin: const EdgeInsets.symmetric(vertical: 1),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              CircleAvatar(
+                radius: 18,
+                backgroundColor: AppColors.greyWhiteColor,
+                child: Icon(
+                  icon,
+                  color: Colors.black45,
+                  size: 15,
+                ),
+              ),
+              const SizedBox(
+                width: 15,
+              ),
+              Text(title),
+            ],
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.arrow_forward_ios,
+              size: 15,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
+
+// TODO: Change the Icons From Network to Assets 
