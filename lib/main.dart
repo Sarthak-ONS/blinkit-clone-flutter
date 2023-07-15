b38738cfe856d505e3b6435bb2b1a289d9cba5ba
Sarthak Agarwal <57105611+Sarthak-ONS@users.noreply.github.com>
2023-07-15 12:31:44 +0530

    Added Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.removeAfter(initialization);
+
   runApp(const MainApp());
+}
+
+Future initialization(BuildContext? context) async {
+  await Future.delayed(const Duration(seconds: 5));
 }
 
 class MainApp extends StatelessWidget {
@@ -10,6 +18,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
